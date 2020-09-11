08d9be8f1029bcf6af0d9fd530ef8a609b12188e
Bryan Macario Coronado <BMac9715@users.noreply.github.com>
2020-09-10 19:34:17 -0600

    Inicio del proyecto

---
+++ clash/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Navbar from './components/Navbar.js'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
