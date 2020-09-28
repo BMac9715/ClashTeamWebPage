3689c8524f0c7b0acf9f536e9dbe3634e406e648
Bryan Macario Coronado <BMac9715@users.noreply.github.com>
2020-09-27 23:29:55 -0600

    SignIn y SignUp
    Pantalla con el signin y signup de la webpage, unicamente parte visual

--- clash/src/App.jsx
+++ clash/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar.js'
 import SideNavBar from './components/sidenavbar/sidenavbar.js'
+import Home from './components/signin/signin.js'
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <SideNavBar/>
+      <Home />
     </div>
   );
 }
